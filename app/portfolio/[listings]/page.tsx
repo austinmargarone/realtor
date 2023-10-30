1c93bd69ba6661f2e2f23b1f61e2072c50eb8f99
fcvirtual <austin@fcvt.dev>
2023-10-30 14:38:42 -0400

    feat: Dynamic Portfolio Route

---
+++ app/portfolio/[listings]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return <div>Dynamic Listings</div>;
+};
+
+export default page;
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,9 +1,26 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
 const page = (props: Props) => {
-  return <div>Portfolio</div>;
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <div>
+        <ul>
+          <li>
+            <Link href="/portfolio/901-long-pond-road">901 Long Pond Road</Link>
+          </li>
+          <li>
+            <Link href="/portfolio/609-wild-mallard-trail">
+              609 Wild Mallard Trail
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default page;
