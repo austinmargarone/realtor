472c7bcad79b189b29cee11e4dedae6eb6dad2a4
fcvirtual <austin@fcvt.dev>
2024-01-02 13:37:34 -0500

    feat: Zod Validation

--- components/contact/Form.tsx
+++ components/contact/Form.tsx
@@ -1,15 +1,59 @@
 "use client";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+// Define the interface for form data
+interface FormData {
+  FirstName: string;
+  LastName: string;
+  Email: string;
+  PhoneNumber: string;
+  PropertyAddress: string;
+  Message: string;
+  buyingProperty: boolean;
+  sellingProperty: boolean;
+}
+
+// Define the Zod schema for form validation
+const formSchema = z.object({
+  FirstName: z.string().min(1).max(15),
+  LastName: z.string().min(1).max(13),
+  Email: z.string().min(1).max(25).email(),
+  PhoneNumber: z.string().min(1).max(14),
+  PropertyAddress: z.string().max(120),
+  Message: z.string().max(200),
+  buyingProperty: z.boolean(),
+  sellingProperty: z.boolean(),
+});
+
+// Create the Zod resolver
+const resolver = zodResolver(formSchema);
+
+// Define the Form component
 const Form = () => {
+  // Destructure useForm hook to get form methods and state
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
-  } = useForm();
-  const onSubmit = (data: any) => console.log(data);
+  } = useForm<FormData>({
+    resolver,
+  });
+
+  // Define the onSubmit function
+  const onSubmit = (data: FormData) => {
+    // Zod validation succeeded, proceed with your logic
+    console.log(data);
+
+    reset();
+  };
+
+  // Log form validation errors
   console.log(errors);
+
   return (
     <section className="flex min-w-[300px] max-w-[420px] grow bg-kw-maingray p-8 dark:bg-kw-black">
       <form
@@ -19,57 +63,30 @@ const Form = () => {
         <input
           type="text"
           placeholder="First name"
-          {...register("FirstName", { required: true, max: 15, min: 1 })}
+          {...register("FirstName")}
         />
-        <input
-          type="text"
-          placeholder="Last Name"
-          {...register("LastName", { required: true, max: 13, min: 1 })}
-        />
-        <input
-          type="text"
-          placeholder="Email"
-          {...register("Email", { required: true, max: 25, min: 1 })}
-        />
+        <input type="text" placeholder="Last Name" {...register("LastName")} />
+        <input type="text" placeholder="Email" {...register("Email")} />
         <input
           type="tel"
           placeholder="Phone number"
-          {...register("PhoneNumber", { required: true, max: 14, min: 1 })}
+          {...register("PhoneNumber")}
         />
         <input
           type="text"
           placeholder="Property Address"
-          {...register("PropertyAddress", {
-            required: true,
-            max: 120,
-            min: 1,
-          })}
+          {...register("PropertyAddress")}
         />
-        <textarea
-          {...register("Message", {
-            required: false,
-            max: 200,
-            maxLength: 200,
-          })}
-          placeholder="Message"
-        />
+        <textarea {...register("Message")} placeholder="Message" />
         <div className="flex flex-col gap-3.5">
           <div className="flex gap-3">
-            <input
-              type="checkbox"
-              placeholder="buyer"
-              {...register("buyingProperty")}
-            />
+            <input type="checkbox" {...register("buyingProperty")} />
             <label htmlFor="buyingProperty" className="dark:text-light-900">
               Buying Property
             </label>
           </div>
           <div className="flex gap-3">
-            <input
-              type="checkbox"
-              placeholder="seller"
-              {...register("sellingProperty")}
-            />
+            <input type="checkbox" {...register("sellingProperty")} />
             <label htmlFor="sellingProperty" className="dark:text-light-900">
               Selling Property
             </label>
