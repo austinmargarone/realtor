eb05af740705c5b3afb0561b5d7fa7cbce99c695
fcvirtual <austin@fcvt.dev>
2024-01-08 15:20:39 -0500

    feat: Link & Button Pulse

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const page = () => {
   return (
     <main className="breakpoint mx-auto my-[2.5rem] justify-center">
-      <h2 className="h1 mb-[1.25rem] dark:text-white">Contact Me</h2>
+      <h2 className="md:h1 h2 mb-[1.25rem] dark:text-white">Contact Me</h2>
       <section className="flex justify-center">
         <Form />
       </section>
--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const page = (props: Props) => {
   return (
     <div className="breakpoint mx-auto mt-[2.5rem] flex flex-col gap-[1.25rem]">
-      <h1 className="h1 dark:text-white">Tools</h1>
+      <h1 className="md:h1 h2 dark:text-white">Tools</h1>
       <div className="mx-auto h-[250px] w-[300px] justify-center xs:h-[275px] xs:w-[325px] sm:h-[400px] sm:w-[500px] md:h-[480px] md:w-[600px] lg:h-[680px] lg:w-[850px] xl:h-[750px] xl:w-[950px]">
         <iframe
           className="h-full w-full rounded-sm"
