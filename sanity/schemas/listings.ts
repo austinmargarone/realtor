94c28af5be4cc33527856db741829e36c161f0b5
fcvirtual <austin@fcvt.dev>
2024-01-10 10:49:43 -0500

    sanity schemas for listings and sold

---
+++ sanity/schemas/listings.ts
@@ -0,0 +1,118 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "propertyListing",
+  title: "Property Listing",
+  type: "document",
+  fields: [
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "string",
+    }),
+    defineField({
+      name: "id",
+      title: "ID",
+      type: "string",
+    }),
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+    }),
+    defineField({
+      name: "address",
+      title: "Address",
+      type: "string",
+    }),
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "string",
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "string",
+    }),
+    defineField({
+      name: "list",
+      title: "List Price",
+      type: "string",
+    }),
+    defineField({
+      name: "sale",
+      title: "Sale Price",
+      type: "string",
+    }),
+    defineField({
+      name: "beds",
+      title: "Bedrooms",
+      type: "number",
+    }),
+    defineField({
+      name: "baths",
+      title: "Bathrooms",
+      type: "number",
+    }),
+    defineField({
+      name: "sqft",
+      title: "Square Feet",
+      type: "string",
+    }),
+    defineField({
+      name: "lot",
+      title: "Lot Size",
+      type: "number",
+    }),
+    defineField({
+      name: "year",
+      title: "Year Built",
+      type: "number",
+    }),
+    defineField({
+      name: "garage",
+      title: "Garage Spaces",
+      type: "number",
+    }),
+    defineField({
+      name: "tour",
+      title: "Virtual Tour",
+      type: "url",
+    }),
+    defineField({
+      name: "embed",
+      title: "Embed Code",
+      type: "url",
+    }),
+    defineField({
+      name: "status",
+      title: "Status",
+      type: "string",
+      options: {
+        list: ["FOR SALE", "SOLD", "PENDING", "OFF MARKET"],
+      },
+    }),
+    defineField({
+      name: "color",
+      title: "Color",
+      type: "string",
+    }),
+    defineField({
+      name: "MLS",
+      title: "MLS Number",
+      type: "string",
+    }),
+  ],
+  preview: {
+    select: {
+      title: "title",
+      author: "author.name",
+      media: "mainImage",
+    },
+    prepare(selection) {
+      const { author } = selection;
+      return { ...selection, subtitle: author && `by ${author}` };
+    },
+  },
+});
---
+++ sanity/schemas/sold.ts
@@ -0,0 +1,83 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "soldProperty",
+  title: "Sold Property",
+  type: "document",
+  fields: [
+    defineField({
+      name: "id",
+      title: "ID",
+      type: "string",
+    }),
+    defineField({
+      name: "address",
+      title: "Address",
+      type: "string",
+    }),
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "string",
+    }),
+    defineField({
+      name: "sale",
+      title: "Sale Price",
+      type: "string",
+    }),
+    defineField({
+      name: "beds",
+      title: "Bedrooms",
+      type: "number",
+    }),
+    defineField({
+      name: "baths",
+      title: "Bathrooms",
+      type: "number",
+    }),
+    defineField({
+      name: "sqft",
+      title: "Square Feet",
+      type: "string",
+    }),
+    defineField({
+      name: "lot",
+      title: "Lot Size",
+      type: "number",
+    }),
+    defineField({
+      name: "year",
+      title: "Year Built",
+      type: "number",
+    }),
+    defineField({
+      name: "garage",
+      title: "Garage Spaces",
+      type: "number",
+    }),
+    defineField({
+      name: "status",
+      title: "Status",
+      type: "string",
+      options: {
+        list: ["SOLD"],
+      },
+    }),
+    defineField({
+      name: "color",
+      title: "Color",
+      type: "string",
+    }),
+    defineField({
+      name: "link",
+      title: "Realtor.com Link",
+      type: "url",
+    }),
+  ],
+  preview: {
+    select: {
+      title: "address",
+      media: "image",
+    },
+  },
+});
