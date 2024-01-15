9c4b23badc785b6698556577c9dff194daada5f8
fcvirtual <austin@fcvt.dev>
2024-01-15 10:42:19 -0500

    text-style

--- components/blog/PostBody.tsx
+++ components/blog/PostBody.tsx
@@ -5,7 +5,7 @@ import imageUrlBuilder from "@sanity/image-url";
 
 interface Props {
   publishedAt: string;
-  body: (BlockContent & { asset?: any })[]; // Update the type declaration of `body` to include the `asset` property
+  body: (BlockContent & { asset?: any })[];
 }
 
 const projectId =
@@ -24,8 +24,12 @@ const PostBody = ({ publishedAt, body }: Props) => {
         {body.map((bodyItem) => (
           <div key={bodyItem._key}>
             {bodyItem._type === "block" && (
-              <div className="body dark:bodydark">
-                {(bodyItem as any).children[0].text}
+              <div className={getBodyStyle(bodyItem.style)}>
+                {bodyItem.children.map((span) => (
+                  <span key={span._key} className={getSpanStyle(span.marks)}>
+                    {span.text}
+                  </span>
+                ))}
               </div>
             )}
             {bodyItem._type === "image" && bodyItem.asset && (
@@ -34,6 +38,7 @@ const PostBody = ({ publishedAt, body }: Props) => {
                 width={250}
                 height={250}
                 src={builder.image(bodyItem.asset._ref).url()}
+                className="mx-auto mb-[1.25rem] h-fit w-[350px]"
               />
             )}
           </div>
@@ -41,6 +46,35 @@ const PostBody = ({ publishedAt, body }: Props) => {
       </div>
     </div>
   );
+};
+
+const getBodyStyle = (style: string) => {
+  // Map your Sanity styles to HTML elements or React components
+  switch (style) {
+    case "h1":
+      return "h1 dark:text-white";
+    case "h2":
+      return "h2 dark:text-white";
+    case "h3":
+      return "h3 dark:text-white";
+    case "h4":
+      return "h4 dark:text-white";
+    default:
+      return "body dark:bodydark"; // Default style
+  }
+};
+
+const getSpanStyle = (marks: string[]) => {
+  // Map your Sanity marks to HTML elements or React components
+  let style = "normal";
+  if (marks.includes("strong")) {
+    style = "bold";
+  }
+  if (marks.includes("em")) {
+    style = "italic";
+  }
+
+  return style;
 };
 
 export default PostBody;
