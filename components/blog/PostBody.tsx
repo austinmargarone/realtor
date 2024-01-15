ec9ae4b3144288ee4e6047a3e530b6523a87a5aa
fcvirtual <austin@fcvt.dev>
2024-01-15 10:30:24 -0500

    fix: Blog Image

--- components/blog/PostBody.tsx
+++ components/blog/PostBody.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { BlockContent } from "@/types/BlockContent";
 import Image from "next/image";
+import imageUrlBuilder from "@sanity/image-url";
 
 interface Props {
   publishedAt: string;
-  body: BlockContent[];
+  body: (BlockContent & { asset?: any })[]; // Update the type declaration of `body` to include the `asset` property
 }
 
+const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "defaultProjectId";
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? "defaultDataset";
+
 const PostBody = ({ publishedAt, body }: Props) => {
+  const builder = imageUrlBuilder({
+    projectId,
+    dataset,
+  });
+
   return (
     <div className="breakpoint mx-auto mt-[1.25rem] py-[2.5rem]">
       <div>
@@ -18,12 +28,12 @@ const PostBody = ({ publishedAt, body }: Props) => {
                 {(bodyItem as any).children[0].text}
               </div>
             )}
-            {bodyItem._type === "image" && (
+            {bodyItem._type === "image" && bodyItem.asset && (
               <Image
-                src={`/${(bodyItem as any).asset._ref}`}
                 alt={(bodyItem as any).alt}
                 width={250}
                 height={250}
+                src={builder.image(bodyItem.asset._ref).url()}
               />
             )}
           </div>
