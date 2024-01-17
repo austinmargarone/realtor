f392e32b9f0a0d4341d396c69dd51c5c48243a02
fcvirtual <austin@fcvt.dev>
2024-01-17 14:47:31 -0500

    feat: market stats

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -22,6 +22,60 @@ const page = (props: Props) => {
           className="mx-auto flex h-full w-[300px] justify-center rounded-sm shadow-lg xs:w-[300px] sm:w-[500px] md:w-[600px] lg:hidden"
         />
       </div>
+      <section className="mx-auto mb-[2.5rem] flex flex-col gap-6">
+        {/* Sales Price - Short Term */}
+        <div className="flex flex-col">
+          <h3 className="h2 pb-[4px] dark:text-white">Sales Price</h3>
+          <iframe
+            width="640"
+            height="480"
+            src="https://grar.stats.showingtime.com/infoserv/s-v1/KVvo-R1F?w=640&amp;h=480"
+            className="mx-auto w-[300px] rounded-sm sm:w-[500px] md:w-[600px]"
+          ></iframe>
+        </div>
+        {/* Sales Price - Long Term */}
+        <div className="flex flex-col">
+          <h3 className="h2 pb-[4px] dark:text-white">
+            Sales Price - Long Term
+          </h3>
+          <iframe
+            width="640"
+            height="480"
+            src="https://grar.stats.showingtime.com/infoserv/s-v1/KVvU-n2S?w=640&amp;h=480"
+            className="mx-auto w-[300px] rounded-sm sm:w-[500px] md:w-[600px]"
+          ></iframe>
+        </div>
+        {/* Price Per SQFT */}
+        <div className="flex flex-col">
+          <h3 className="h2 pb-[4px] dark:text-white">Price Per SQFT</h3>{" "}
+          <iframe
+            width="640"
+            height="480"
+            src="https://grar.stats.showingtime.com/infoserv/s-v1/KVvX-sqN?w=640&amp;h=480"
+            className="mx-auto w-[300px] rounded-sm sm:w-[500px] md:w-[600px]"
+          ></iframe>
+        </div>
+        {/* Days on Market */}
+        <div className="flex flex-col">
+          <h3 className="h2 pb-[4px] dark:text-white">Days on Market</h3>
+          <iframe
+            width="640"
+            height="480"
+            src="https://grar.stats.showingtime.com/infoserv/s-v1/KVvR-rDA?w=640&amp;h=480"
+            className="mx-auto w-[300px] rounded-sm sm:w-[500px] md:w-[600px]"
+          ></iframe>
+        </div>
+        {/* Homes For Sale */}
+        <div className="flex flex-col">
+          <h3 className="h2 pb-[4px] dark:text-white">Inventory</h3>
+          <iframe
+            width="640"
+            height="480"
+            src="https://grar.stats.showingtime.com/infoserv/s-v1/KVvq-hOW?w=640&amp;h=480"
+            className="mx-auto w-[300px] rounded-sm sm:w-[500px] md:w-[600px]"
+          ></iframe>
+        </div>
+      </section>
     </div>
   );
 };
