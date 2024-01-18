3c02a62ae7f83fe2b3eefb2233b39bef4bb40334
fcvirtual <austin@fcvt.dev>
2024-01-18 14:28:51 -0500

    fix: overflow

--- components/home/Cover.tsx
+++ components/home/Cover.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Cover = () => {
   return (
     <main
-      className="flex min-h-screen w-screen items-center justify-center bg-cover bg-center"
+      className="flex min-h-screen  items-center justify-center bg-cover bg-center"
       style={{ backgroundImage: "url('/roccover.jpg')" }}
     >
       <h1 className="h1 z-10 text-4xl text-white">Your Local Realtor</h1>
