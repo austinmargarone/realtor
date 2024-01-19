2525210834da79d8e8cb878b3ef6ace5adf3b3f4
fcvirtual <austin@fcvt.dev>
2024-01-19 14:45:27 -0500

    Next Config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
+    path: "/_next/image",
   },
+  basePath: "/",
 };
 
 module.exports = nextConfig;
