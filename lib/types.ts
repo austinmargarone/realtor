05bffe9d53cb2d00e46595e06fdf403e5a948831
fcvirtual <austin@fcvt.dev>
2024-03-02 11:06:58 -0500

    fix: Form validation

--- lib/types.ts
+++ lib/types.ts
@@ -4,22 +4,22 @@ export const formSchema = z.object({
   FirstName: z
     .string()
     .min(1, { message: "Please enter a valid first name." })
-    .max(15),
+    .max(50),
   LastName: z
     .string()
     .min(1, { message: "Please enter a valid last name." })
-    .max(13),
+    .max(50),
   Email: z
     .string()
     .min(1, { message: "Please enter a valid email address." })
-    .max(25)
+    .max(125)
     .email(),
   PhoneNumber: z
     .string()
     .min(1, { message: "Please enter a valid phone number." })
-    .max(14),
-  PropertyAddress: z.string().max(120),
-  Message: z.string().max(200),
+    .max(25),
+  PropertyAddress: z.string().max(250),
+  Message: z.string().max(1000),
   buyingProperty: z.boolean(),
   sellingProperty: z.boolean(),
 });
