5bff127361eb6e039dea7af50a9efc26fc59ee69
austinmargarone <austin@fcvt.dev>
2024-10-06 20:37:35 -0400

    Update: Text

--- components/listing/ListYours.tsx
+++ components/listing/ListYours.tsx
@@ -7,10 +7,10 @@ const ListYours = () => {
       <div className="breakpoint mx-auto my-[1.25rem] flex flex-col items-center justify-center gap-[1.25rem]">
         <h2 className="h2 dark:text-white">List Your Property</h2>
         <p className="body dark:bodydark flex max-w-[600px] text-center">
-          Selling your home is an art, and I amm meticulous about the details.
-          Your needs are at the forefront of every decision, from the strategic
-          pricing that reflects your properties value to the tailored marketing
-          campaigns that reach the right audience.
+          Selling a home requires precision, and I’m dedicated to every detail.
+          From setting the ideal price that highlights your property’s true
+          value to crafting customized marketing strategies that connect with
+          the perfect buyers, your goals are always my top priority.
         </p>
         <Link href={"/contact"}>
           <button className="button dark:buttondark">Contact Me</button>
